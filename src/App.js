d0257b67d0191abaa48a22ab363378fcca877150
Fabio Tenorio <ftenorio@voyageprive.com>
2022-11-28 01:59:47 +0100

    first elements for the answer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import './GetData';
+import Modal from './Modal';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [data, setData] = useState([]);
+
+  const getData = () => {
+      fetch('data.json'
+      ,{
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+          }
+      }
+      )
+        .then(function(response) {
+          console.log(response)
+          return response.json();
+        })
+        .then(function(myJson) {
+          setData(myJson)
+        });
+    }
+    useEffect(() => {
+      getData()
+    },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Modal show={handleShow}>
+      </Modal>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>lastname</th>
+            <th>firstname</th>
+            <th>age</th>
+            <th>city</th>
+            <th>address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(row =>
+          <tr>
+            {Object.values(row).map(key => <td>{key}</td>)}
+          </tr>)}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/Modal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Modal() {
+    return (
+        <div>
+            <h1>Table loaded</h1>
+            <h2>This message shall disappear in 10 seconds</h2>
+        </div>
+    )
+}
+
+export default Modal;
